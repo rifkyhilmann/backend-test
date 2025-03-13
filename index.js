27b68928691fdd0beb3d9ba00d50ea6f03fd2e84
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2025-03-13 15:29:42 +0700

    feat: update

--- index.js
+++ index.js
@@ -77,4 +77,6 @@ app.delete("/posts/:id", async (req, res) => {
     }
 });
 
-app.listen(3001, () => console.log("Server running on port 3001"));
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log("Server running on port 3001"));
